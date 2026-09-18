789c71731751a2c19febe3aaf6e3c08f98d241a3
Egor Ermakov <unplaced@gmail.com>
2026-09-18 11:48:21 +0100

    Basic working lexer

---
+++ java/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A flake for java based development";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.jdt-language-server
+            pkgs.jdk25
+            pkgs.gradle_9
+
+          ];
+        };
+      }
+    );
+}
